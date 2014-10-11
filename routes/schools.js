bc060a7587410dfb8f7a43213575d855357a25ec
Dipesh Shah <dipesh1005@gmail.com>
2014-10-11 11:23:12 +0530

    add school Ui

--- routes/schools.js
+++ routes/schools.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('addSchool', { title: 'New' });
+  res.render('addSchool', { title: 'PayUp' });
 });
 
 module.exports = router;
